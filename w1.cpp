102dc8c71d70a072ee38e20e718fa7ee97560894
Amrkurrus <66263896+Amrkurrus@users.noreply.github.com>
2022-10-01 20:03:36 +0600

    Add files via upload

---
+++ w1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int x;
+	double sol;
+	cin >> x;
+	sol = pow(x, 2) - 7 * x + 6;
+	cout << "solution is " << sol << endl;
+}
