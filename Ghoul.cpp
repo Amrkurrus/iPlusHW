fc49378e45dab4af855a3adbfb2ae7c9cac38c89
Amrkurrus <66263896+Amrkurrus@users.noreply.github.com>
2022-10-04 15:20:43 +0600

    Add files via upload

---
+++ Ghoul.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	string text = "?    LET ME DIE!!! I don't really know what the point in life is everybody knows that we won't survive...";
+
+	for (int i = 100; i > 0; i--)
+	{
+		cout << "Я..." << endl;
+		Sleep(5);
+	}
+
+	for (int g = 100; g > 0; g--)
+	{
+		cout << "Гуль!!!" << endl;
+		Sleep(5);
+	}
+
+	for (int num = 1000; num > 0; num -= 7)
+	{
+		cout << num << " - 7 = " << num - 7 << text << endl;
+		Sleep(50);
+
+	}
+
+	cin.get();
+	return 0;
+}
